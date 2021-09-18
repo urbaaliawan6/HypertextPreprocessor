f8b34d810a5be8b29e4e53162bc8ec3ebfe3c7b2
Urba Ali Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-09-17 21:55:53 -0700

    Delete Projects directory

--- Projects/select-2.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('config.php');
-$result=mysqli_query($db,"select* from star where username='adil'" );
-$result=mysqli_query($db,"select* from star where password='2345'" );
-$result=mysqli_fetch_array($result);
-echo $result['username'];
-echo "<br>";
-echo $result['password'];
-?>
