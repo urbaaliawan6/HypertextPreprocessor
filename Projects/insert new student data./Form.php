06ac162aead5ad24f25e5f341d1b7e6d44bf6eae
Urba Ali Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-09-17 22:04:23 -0700

    Add files via upload

---
+++ Projects/insert new student data./Form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Form</title>
+</head>
+<body>
+<center>
+<h1>Registration Form</h1><hr>
+<form action="reg_check.php" method="post">
+<h5>S.No</h5>
+<input type="text" name="sno" placeholder="Enter your s.no"><br>
+<h5>Name</h5>
+<input type="text" name="name" placeholder="Enter your name"><br>
+<h5>Father Name</h5>
+<input type="text" name="fname" placeholder="Enter your fathername"><br>
+<h5>Roll No</h5>
+<input type="text" name="rollno" placeholder="Enter your rollno"><br><br>
+<h5>Class</h5>
+<input type="text" name="class" placeholder="Enter your class"><br><br>
+<input type="submit" value="Submit" name="submit">
+</center>
+</form>
+</body>
+</html>
