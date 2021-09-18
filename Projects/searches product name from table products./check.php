977291332b7961b6e6063e073786f1bce9a67c87
Urba Ali Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-09-17 21:20:23 -0700

    Add files via upload

---
+++ Projects/searches product name from table products./check.php
@@ -0,0 +1,10 @@
+<?php
+$a= $_REQUEST['name'];
+$b= $_REQUEST['email'];
+$c= $_REQUEST['password'];
+echo "$a";
+echo "<br>";
+echo "$b";
+echo "<br>";
+echo "$c";
+?>
---
+++ Projects/searches product name from table products./config.php
@@ -0,0 +1,9 @@
+<?php
+#config.php
+define('DB_SERVER','127.0.0.1:3307');
+// define('DB_SERVER','localhost:8080');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'star');
+$db=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
